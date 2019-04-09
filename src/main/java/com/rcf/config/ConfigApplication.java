478b9b350819bedb084de3db2264410e707ecf9f
Ramón Cigüenza <rcifuster@gmail>
2019-04-09 11:40:33 +0200

    Initial Version

---
+++ src/main/java/com/rcf/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.rcf.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+@EnableAutoConfiguration
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
