2cf2ee398a3e218335e8d4009b3f53ff3ca7caad
Ramón Cigüenza <rcifuster@gmail>
2019-04-09 13:13:51 +0200

    Initial configuration

--- src/main/java/com/rcf/config/ConfigApplication.java
+++ src/main/java/com/rcf/config/ConfigApplication.java
@@ -1,19 +1,18 @@
 package com.rcf.config;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@Configuration
-@EnableConfigServer
-@EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(ConfigApplication.class)
+				.properties("spring.config.name:configserver")
+				.build()
+				.run(args);
 	}
 
 }
