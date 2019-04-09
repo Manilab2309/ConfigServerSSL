de2833e0630303d0b8a35121a90ee7c04e1a3173
Ramón Cigüenza <rcifuster@gmail>
2019-04-09 17:28:54 +0200

    Final version with HTTP

--- src/main/java/com/rcf/config/ConfigApplication.java
+++ src/main/java/com/rcf/config/ConfigApplication.java
@@ -1,10 +1,11 @@
 package com.rcf.config;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
